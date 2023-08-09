ce349ff9eea43ff6f872215fb53fb391ac5e80b9
zhouxd <dedsecezio@163.com>
2023-08-09 15:50:32 +0800

    小样本微调代码

---
+++ label/finetune.bat
@@ -0,0 +1,24 @@
+python finetune.py  ^
+    --device gpu ^
+    --logging_steps 10 ^
+    --save_steps 100 ^
+    --eval_steps 100 ^
+    --seed 1000 ^
+    --model_name_or_path uie-base ^
+    --output_dir ./checkpoint/model_best ^
+    --train_path data/train.txt ^
+    --dev_path data/dev.txt  ^
+    --max_seq_len 512  ^
+    --per_device_train_batch_size  4 ^
+    --per_device_eval_batch_size 4 ^
+    --num_train_epochs 4 ^
+    --learning_rate 1e-5 ^
+    --do_train ^
+    --do_eval ^
+    --do_export ^
+    --export_model_dir ./checkpoint/model_best ^
+    --overwrite_output_dir ^
+    --disable_tqdm True ^
+    --metric_for_best_model eval_f1 ^
+    --load_best_model_at_end  True ^
+    --save_total_limit 1
