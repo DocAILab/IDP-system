81b68a582ba282de7675c04a69d97959febb4127
郝鹏程 <20373487@buaa.edu.cn>
2023-08-11 16:12:32 +0800

    添加read_file接口到api.py

--- api.py
+++ api.py
@@ -1,5 +1,38 @@
 from paddlenlp import Taskflow
 from uie_schemas import schemas_dict_education, schemas_dict_education_D, sensitive_words
+from utils import *
+
+
+def read_file(file_path, ocr, table_engine, table_extract=False, table_dir=None):
+    """
+        读入pdf/docx文件，获取文字内容
+    :param file_path: 文件绝对路径
+    :param ocr: OCR模型
+    :param table_engine: 版面识别模型
+    :param table_extract: 是否提取表格到excel
+    :param table_dir: 表格存储目录
+    :return: 原始文件名，str；提取文件名，str；文字读取结果，str
+    """
+
+    if not os.path.exists(file_path):
+        raise ValueError('file path is not exist!', file_path)
+    if not os.path.isfile(file_path):
+        raise ValueError('not a correct file path!', file_path)
+    if not file_path.lower().endswith(".pdf") and not file_path.lower().endswith(".docx"):
+        raise ValueError('not a supported file type! (pdf/docx)', file_path)
+
+    # 获取无标题文件的标题
+    ori_filename, filename = get_filename(file_path, ocr, table_engine)  # 不包含后缀，且删除非法字符的文件名
+
+    # 获取文字内容
+    content = get_text(file_path, ocr)
+
+    # 单独提取表格
+    if table_extract:
+        extract_table(file_path, table_dir)
+
+    return ori_filename, filename, content
+
 
 class Info_Extraction:
     def __init__(self, schemas_dict= schemas_dict_education, model= 'uie-m-base', task_path= None, use_fast= True):
